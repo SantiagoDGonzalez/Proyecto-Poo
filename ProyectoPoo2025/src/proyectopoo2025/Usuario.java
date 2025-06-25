5050ec3bcd8cd221018b11cdb5b21be4418b1442
Cristian <cristianalejandro172431@gmail.com>
2025-06-24 23:47:21 -0500

    Añadido metodos y atributos a evento

--- ProyectoPoo2025/src/proyectopoo2025/Usuario.java
+++ ProyectoPoo2025/src/proyectopoo2025/Usuario.java
@@ -6,21 +6,5 @@ public class Usuario {
     private String nombreUsuario;
     private String contraseña;
     private String email;
-    
-    Usuario (int id, String nombreUsuario, String contraseña, String email){
-        this.id = id;
-        this.nombreUsuario = nombreUsuario;
-        this.contraseña = contraseña;
-        this.email = email;
-    }
-    
-    public boolean autentificar(){
-        
-    }
-    public boolean registrar () {
-        
-    }
-    public void actualzarPerfil(){
-        
     }
 }
